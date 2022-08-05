a8810e2ae4a850b01a91259d0e9ae23c252bec3a
Donci31 <szigetidonat@gmail.com>
2022-08-05 22:07:39 +0200

    Lazy download

--- musicbot/MusicBot.py
+++ musicbot/MusicBot.py
@@ -5,7 +5,6 @@ from tempfile import TemporaryDirectory
 from pytube import YouTube, Playlist
 
 from musicbot import utils
-from musicbot.Song import Song
 from musicbot.utils import YOUTUBE_WATCH_REGEX, YOUTUBE_PLAYLIST_REGEX
 
 
@@ -47,9 +46,9 @@ class MusicBot(commands.Bot):
             channel = ctx.channel
 
             if self.song_queues[guild_id]:
-                numbered_list = '\n'.join([f'**{i})** [{song.song_title}]({utils.get_url(song.song_id)}) '
-                                           f'``{utils.time_format(song.song_length)}``'
-                                           for i, song in enumerate(self.song_queues[guild_id], 1)])
+                numbered_list = '\n'.join([f'**{i})** [{song.title}]({utils.get_url(song.video_id)}) '
+                                           f'``{utils.time_format(song.length)}``'
+                                           for i, song in enumerate(self.song_queues[guild_id][:10], 1)])
                 await utils.send_embed(channel=channel, title='Queue', description=numbered_list)
 
         @self.command()
@@ -84,38 +83,32 @@ class MusicBot(commands.Bot):
         await utils.send_embed(channel=channel, title='Songs queued', description=desc)
 
         for video in playlist.videos:
-            utils.download_song(video, path=self.song_directory.name)
-            new_song = Song(voice, video.video_id, video.title, video.length)
-            self._add_to_queue(guild_id, new_song)
+            self._add_to_queue(voice, guild_id, video)
 
     async def _add_song(self, ctx, video):
         voice = ctx.voice_client
         guild_id = ctx.guild.id
         channel = ctx.channel
 
-        utils.download_song(video, path=self.song_directory.name)
-        new_song = Song(voice, video.video_id, video.title, video.length)
-
-        desc = (f'[{new_song.song_title}]({utils.get_url(new_song.song_id)}) '
-                f'``{utils.time_format(new_song.song_length)}``')
+        desc = (f'[{video.title}]({utils.get_url(video.video_id)}) '
+                f'``{utils.time_format(video.length)}``')
         await utils.send_embed(channel=channel, title='Song queued', description=desc)
 
-        self._add_to_queue(guild_id, new_song)
+        self._add_to_queue(voice, guild_id, video)
 
-    def _add_to_queue(self, guild_id, new_song):
+    def _add_to_queue(self, voice, guild_id, new_song):
         self.song_queues[guild_id].append(new_song)
-        voice = new_song.voice
 
         if not voice.is_playing():
-            self._start_playing(guild_id)
+            self._start_playing(voice, guild_id)
 
-    def _start_playing(self, guild_id):
+    def _start_playing(self, voice, guild_id):
         if self.song_queues[guild_id]:
             new_song = self.song_queues[guild_id].pop(0)
-            song_path = f'{self.song_directory.name}/{new_song.song_id}.mp4'
-            voice = new_song.voice
+            utils.download_song(new_song, self.song_directory.name)
+            song_path = f'{self.song_directory.name}/{new_song.video_id}.mp4'
 
-            voice.play(FFmpegPCMAudio(song_path), after=lambda e: self._start_playing(guild_id))
+            voice.play(FFmpegPCMAudio(song_path), after=lambda e: self._start_playing(voice, guild_id))
 
     async def close(self):
         self.song_directory.cleanup()
