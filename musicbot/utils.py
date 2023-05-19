fffba179d3b513c4acdfe5c978f85ffda733d018
Donci31 <szigetidonat@gmail.com>
2023-05-19 11:14:11 +0200

    Better syntax for optional values

--- musicbot/utils.py
+++ musicbot/utils.py
@@ -2,7 +2,6 @@ import httpx
 import re
 import time
 from re import Match
-from typing import Optional
 
 YOUTUBE_PLAYLIST_REGEX = re.compile(r'(?:http?s?://)?(?:www\.|m\.)?(?:music.)?youtu\.?be(?:\.com)?'
                                     r'(?:\w*.?://)?\w*.?\w*-?.?\w*(?:playlist|list|embed|.*/)?\??'
@@ -13,10 +12,10 @@ YOUTUBE_WATCH_REGEX = re.compile(r'(?:http?s?://)?(?:www\.|m\.)?(?:music.)?youtu
                                  r'(?:feature=\w*\.?\w*)?&?(?:\?v=|/)(?P<youtube_id>[\w-]{11})(?:\S+)?')
 
 
-def keyword_search(keyword: str) -> Optional[Match[str]]:
+def keyword_search(keyword: str) -> Match[str] | None:
     search_query = {'search_query': keyword}
-    html = httpx.get('https://www.youtube.com/results', params=search_query).text
-    youtube_match = re.search(r'/(?:watch\?v=|shorts/)(?P<youtube_id>[\w-]{11})', html)
+    response = httpx.get('https://www.youtube.com/results', params=search_query)
+    youtube_match = re.search(r'/(?:watch\?v=|shorts/)(?P<youtube_id>[\w-]{11})', response.text)
 
     return youtube_match
 
